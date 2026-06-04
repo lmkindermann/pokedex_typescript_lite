09fed54e43eb7de50e813fa1f7ff20717ff556c4
lmkindermann <lmkindermann@gmail.com>
2026-06-04 12:18:52 -0300

    feat: interface PokemonResume desenvolvida

---
+++ src/models/Pokemon.ts
@@ -0,0 +1,10 @@
+interface PokemonResume{
+    id: number;
+    name: string;
+    height: number;
+    weight: number;
+    types: string[];
+    hp: number;
+    attack: number;
+    defense: number;
+}
