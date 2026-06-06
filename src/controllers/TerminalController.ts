c544c104eaff295bc420ec20f9d5a82b528614d5
lmkindermann <lmkindermann@gmail.com>
2026-06-06 18:06:16 -0300

    feat: implementado listarCatalogo e ajustes em adicionarAoCatalogo

--- src/controllers/TerminalController.ts
+++ src/controllers/TerminalController.ts
@@ -1,36 +1,46 @@
 import { type PokemonResume } from "../models/Pokemon"
+//descomentar para os testes:
+//import { buscaPokemon, type PokeID } from "../services/PokeApiService" 
 
 export let catalogo: PokemonResume[] = [];
 
 export function adicionarAoCatalogo(
     lista:PokemonResume[], 
     pokemon:PokemonResume
-): string { //retornar mensagem, checar se precisa retornar a lista em outro nivel da aplicação
-    let status:string = ''
+): void {
     if(pokemon === null){
-        status = `[ERRO] Entrada inválida.`        
+        console.log(`[ERRO] Entrada inválida.`)
     }
     else if( lista.find((p:PokemonResume) => p.id === pokemon.id) ){
-        status = `[AVISO] ${pokemon.name} já está no catalogo.`    
+        console.log(`[AVISO] ${pokemon.name} já está no catalogo.`)
     } else {
         lista.push(pokemon)    
-        status = `[OK] ${pokemon.name} adicionado ao catalogo.`
+        console.log(`[OK] ${pokemon.name} adicionado ao catalogo.`)
     }
-    return status
 }
+
+export function listarCatalogo(lista: PokemonResume[]) : void {
+    lista.forEach(pokemon => {
+        console.log(`#${pokemon.id} - ${pokemon.name} | Tipos: ${pokemon.types} | Altura: ${pokemon.height} | Peso: ${pokemon.weight} | HP: ${pokemon.hp} | Ataque: ${pokemon.attack} | Defesa: ${pokemon.defense}`)
+    });
+}
+
 
 //Teste
 // export async function catalogoPokemon() {
 //     for(let i=0; i<=2; i++){
 //         let id:PokeID = (Math.random() * 150).toFixed(0)
 //         const resposta:any = await buscaPokemon(id)
-//         //catalogo.push(resposta) //rf
-//         const msg:string = adicionarAoCatalogo(catalogo,resposta)
-//         console.log(msg)
+//         //catalogo.push(resposta)
+//         //const msg:string = adicionarAoCatalogo(catalogo,resposta)
+//         adicionarAoCatalogo(catalogo,resposta)
+//         //console.log(msg)
 //     }
-//     console.log(catalogo)
-//     console.log("posição 2")
-//     console.log(catalogo[1])
+//     //console.log(catalogo)
+//     //console.log("posição 2")
+//     //console.log(catalogo[1])
+//     //console.log("chamando listarCatalogo aqui...")
+//     listarCatalogo(catalogo)
 // }
 // catalogoPokemon()
 
